d0d3ab95ea336c8847fa1675c4aff76e6b90f674
Byuxavi <u175484601@byui.edu>
2025-01-15 10:17:29 +0100

    temple

---
+++ temples.js
@@ -0,0 +1,17 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+// 2. Responsive Hamburger Menu
+const nav = document.querySelector('nav'); 
+const hamburgerButton = document.createElement('button'); 
+hamburgerButton.setAttribute('id', 'hamburger'); 
+hamburgerButton.textContent = '☰'; 
+document.querySelector('header').appendChild(hamburgerButton); 
+
+
+hamburgerButton.addEventListener('click', () => {
+nav.classList.toggle('visible'); 
+hamburgerButton.textContent = nav.classList.contains('visible') ? '✖' : '☰'; });
--- week02/scripts/book.js
+++ week02/scripts/book.js
@@ -3,20 +3,20 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button'); 
 const list = document.querySelector('#chapterList'); 
 
-const li = document.createElement('li');
-
-const deleteButton = document.createElement('button');
-deleteButton.textContent = '❌';
-
-li.textContent = input.value;
-li.append(deleteButton);
-list.append(li);
-
-deleteButton.addEventListener('click', () => {
-    list.removeChild(li);
-});
-
-input.value = '';
-input.focus();
-
+button.addEventListener('click', () => {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li'); 
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌'; 
+        li.textContent = input.value.trim(); 
+        li.append(deleteButton); 
+        list.append(li); 
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li); 
+            input.focus();   });
+        input.value = '';
+    } else {
+        alert('Please enter a chapter!'); 
+    }
+    input.focus();});
 
